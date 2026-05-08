206dcad6f9c9be65f04ea2efbafd1eca287c56e2
winstone-1 <winstonemuna404@gmail.com>
2026-05-08 10:00:43 +0300

    Add 'Blogs' link to navigation bar

--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Blog
 
-# Create your views here.
 def index(request):
-    return render(request, 'index.html') 
+    context = {'name': 'John Doe'}
+    return render(request, 'index.html', context)
+
+def about(request):
+    
+    return render(request, 'about.html')
+
+def contact(request):
+    
+    return render(request, 'contact.html')
+
+def bloglist(request):    
+    blogs = Blog.objects.all()
+    context = {'blogs': blogs}
+    return render(request, 'blog_list.html', context)
